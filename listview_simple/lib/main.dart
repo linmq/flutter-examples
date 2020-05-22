fdd3eedd3cf008c20a2f96b5e609b222efa00c70
linmq <leolmq@gmail.com>
2020-05-22 16:23:27 +0800

    listview_simple Migrate to AndroidX

---
+++ listview_simple/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter ListView Simple Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: ListView(
+          children: const <Widget>[
+            Card(child: ListTile(title: Text('One-line ListTile'))),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with leading widget'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('One-line with trailing widget'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with both widgets'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('One-line dense ListTile'),
+                dense: true,
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(size: 56.0),
+                title: Text('Two-line ListTile'),
+                subtitle: Text('Here is a second line'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(size: 72.0),
+                title: Text('Three-line ListTile'),
+                subtitle: Text(
+                  'A sufficiently long subtitle warrants three lines.'
+                ),
+                trailing: Icon(Icons.more_vert),
+                isThreeLine: true,
+              ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
