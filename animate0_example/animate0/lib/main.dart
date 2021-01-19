1baac07c27f78e46f2bb62d494508d550c5bb596
linmq <leolmq@gmail.com>
2021-01-19 15:51:11 +0800

    animation

--- animate0_example/animate0/lib/main.dart
+++ animate0_example/animate0/lib/main.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
 
 void main() => runApp(LogoApp());
 
 class LogoApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
 }
+           
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {            
+  Animation<double> animation;            
+  AnimationController controller;            
 
-class _LogoAppState extends State<LogoApp> {
+  @override            
+  void initState() {            
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(() {
+        setState(() {
+          // The state that has changed here is the animation object’s value.
+        });
+      });
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
         margin: EdgeInsets.symmetric(vertical: 10),
-        height: 300,
-        width: 300,
+        height: animation.value,
+        width: animation.value,
         child: FlutterLogo(),
       ),
     );
+  }
+
+  @override            
+  void dispose() {     
+    controller.dispose();
+    super.dispose();
   }
 }
