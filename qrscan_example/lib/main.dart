c7d90d6cbbe5a7bb7a0140d6aa2343d7445dd36b
linmq <leolmq@gmail.com>
2021-07-13 18:12:15 +0800

    upgrade to null safety

--- qrscan_example/lib/main.dart
+++ qrscan_example/lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 import 'dart:typed_data';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
 
 void main() {
@@ -14,54 +15,88 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String barcode = '';
-  Uint8List bytes = Uint8List(200);
+  Uint8List bytes = Uint8List(0);
+  TextEditingController? _outputController;
 
   @override
   initState() {
     super.initState();
+    this._outputController = new TextEditingController();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Qrcode Scanner Example'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                width: 200,
-                height: 200,
-                child: Image.memory(bytes),
-              ),
-              Text('RESULT  $barcode'),
-              RaisedButton(onPressed: _scan, child: Text("Scan")),
-              RaisedButton(onPressed: _scanPhoto, child: Text("Scan Photo")),
-              RaisedButton(onPressed: _generateBarCode, child: Text("Generate Barcode")),
-            ],
-          ),
+        backgroundColor: Colors.grey[300],
+        body: Builder(
+          builder: (BuildContext context) {
+            return ListView(
+              children: <Widget>[
+                Container(
+                  color: Colors.white,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20),
+                      TextField(
+                        controller: this._outputController,
+                        maxLines: 2,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.wrap_text),
+                          helperText:
+                              'The barcode or qrcode you scan will be displayed in this area.',
+                          hintText:
+                              'The barcode or qrcode you scan will be displayed in this area.',
+                          hintStyle: TextStyle(fontSize: 15),
+                          contentPadding:
+                              EdgeInsets.symmetric(horizontal: 7, vertical: 15),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      this._buttonGroup(),
+                      SizedBox(height: 70),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
   }
 
+  Widget _buttonGroup() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: 120,
+            child: InkWell(
+              onTap: _scan,
+              child: Card(
+                child: Column(
+                  children: <Widget>[
+                    Divider(height: 20),
+                    Expanded(flex: 1, child: Text("Scan")),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   Future _scan() async {
-    String barcode = await scanner.scan();
-    setState(() => this.barcode = barcode);
-  }
-
-  Future _scanPhoto() async {
-    String barcode = await scanner.scanPhoto();
-    setState(() => this.barcode = barcode);
-  }
-
-  Future _generateBarCode() async {
-    Uint8List result = await scanner.generateBarCode('https://github.com/leyan95/qrcode_scanner');
-    this.setState(() => this.bytes = result);
+    await Permission.camera.request();
+    String? barcode = await scanner.scan();
+    if (barcode == null) {
+      print('nothing return.');
+    } else {
+      this._outputController!.text = barcode;
+    }
   }
 }
